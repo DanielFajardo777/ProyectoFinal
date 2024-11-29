dc481f72d59ce8786bb88f655b11c44722038d08
Daniel <daniel.fajardo35757@ucaldas.edu.co>
2024-11-29 14:46:58 -0500

    consumo de api y tambien creacion de cards con la Fake Store

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,30 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { MiServicio } from './fake-store.service';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [HttpClientModule], // Only import HttpClientModule
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css'],
+  providers: [HttpClient], // Add HttpClient to providers
+
 })
-export class AppComponent {
-  title = 'proy';
+export class AppComponent implements OnInit {
+  products: any[] = [];
+
+  constructor(private miServicio: MiServicio) {}
+
+  ngOnInit(): void {
+    this.miServicio.getData().subscribe(data => {
+      this.products = data;
+    });
+    this.uploadedData();
+  }
+
+  uploadedData() {
+    console.log(this.products);
+  }
 }
